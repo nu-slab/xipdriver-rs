2eec1037c3a10863544e2766912d1e30d39ef317
shinmai1054 <matsui1054@icloud.com>
2023-05-23 18:05:39 +0900

    Video Frame Buffer Writeの実装

--- examples/v_frmbuf.rs
+++ examples/v_frmbuf.rs
@@ -7,9 +7,18 @@ fn main() {
         "udmabuf_vfbr",
     )
     .unwrap();
+
     let mut csc =
         xipdriver_rs::v_proc_ss::VideoProcSubsystemCsc::new(&map["/v_proc_ss_0"], "v_proc_ss")
             .unwrap();
+
+    let mut vfb_w = xipdriver_rs::v_frmbuf::VideoFrameBufWrite::new(
+        &map["/v_frmbuf_wr_0"],
+        "v_frmbuf_wr",
+        "udmabuf_vfbw",
+    )
+    .unwrap();
+
     let gpio = xipdriver_rs::axigpio::AxiGpio::new(&map["/axi_gpio_0"], "gpio").unwrap();
 
     println!("{:06x}", gpio.read_data(1).unwrap());
@@ -17,7 +26,7 @@ fn main() {
     let frame_width = 1280;
     let frame_height = 720;
 
-    // v_frmbuf config
+    // v_frmbuf_read config
     vfb_r.image_width = frame_width;
     vfb_r.image_height = frame_height;
     vfb_r.set_format("YUYV");
@@ -25,30 +34,64 @@ fn main() {
     vfb_r.write_format();
     vfb_r.set_framebuf_addr();
 
-    // v_frmbuf write test
-    let frame: [u8; 1280 * 720 * 2] = [0x40; 1280 * 720 * 2];
-    vfb_r.write_frame(&frame);
-    println!("vfb_r.is_ready: {}", vfb_r.is_ready());
-    vfb_r.start();
-    println!("vfb_r.is_ready: {}", vfb_r.is_ready());
-
     // v_proc config
     csc.set_frame_size(frame_width, frame_height);
     csc.set_mode(2, 0).unwrap();
     csc.set_auto_restart_enable(true);
 
-    println!("csc.is_ready: {}", csc.is_ready());
-    csc.start().unwrap();
-    println!("csc.is_ready: {}", csc.is_ready());
-    let csc_mat = csc.read_csc_matrix();
-    for c in csc_mat.iter() {
-        println!("{} ", c);
-    }
+    // v_frmbuf_write config
+    vfb_w.image_width = frame_width;
+    vfb_w.image_height = frame_height;
+    vfb_w.set_format("RGB8");
+    vfb_w.set_auto_restart_enable(true);
+    vfb_w.write_format();
+    vfb_w.set_framebuf_addr();
 
-    // read pixel data
-    for _ in 1..100 {
-        println!("{:06x}", gpio.read_data(1).unwrap());
+    // generate test frame
+    let frame: Vec<u8> = vec![0xFF; 1280 * 720 * 3];
+    let frame_yuyv = rgb2yuyv(&frame);
+    for i in 0..10 {
+        print!("{} ", frame_yuyv[i]);
     }
+    println!();
+
+    // write frame to v_frmbuf_read
+    vfb_r.write_frame(&frame_yuyv);
+
+    // start IPs
+    vfb_r.start();
+    csc.start().unwrap();
+    vfb_w.start();
+
+    println!("read");
+    let rgb_frame = vfb_w.read_frame();
+    println!("save");
+    rgb_frame.save("out.png").unwrap();
+    println!("done");
+
     vfb_r.set_auto_restart_enable(false);
     csc.set_auto_restart_enable(false);
+    vfb_w.set_auto_restart_enable(false);
+}
+
+fn rgb2yuyv(rgb: &[u8]) -> Vec<u8> {
+    assert_eq!(rgb.len() % 3, 0);
+    let length = rgb.len() / 3;
+    let mut yuyv = vec![0; length * 2];
+    for i in 0..length {
+        let r = rgb[3 * i + 0] as f64;
+        let g = rgb[3 * i + 1] as f64;
+        let b = rgb[3 * i + 2] as f64;
+        let y = 0.257 * r + 0.504 * g + 0.098 * b + 16.0;
+        let u = -0.148 * r - 0.291 * g + 0.439 * b + 128.0;
+        let v = 0.439 * r - 0.368 * g - 0.071 * b + 128.0;
+        if i % 2 == 0 {
+            yuyv[2 * i + 0] = y.max(0.0).min(255.0) as u8;
+            yuyv[2 * i + 1] = u.max(0.0).min(255.0) as u8;
+        } else {
+            yuyv[2 * i + 0] = y.max(0.0).min(255.0) as u8;
+            yuyv[2 * i + 1] = v.max(0.0).min(255.0) as u8;
+        }
+    }
+    yuyv
 }
--- src/v_frmbuf.rs
+++ src/v_frmbuf.rs
@@ -4,8 +4,6 @@ use anyhow::{ensure, Result};
 #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
 use jelly_mem_access::*;
 
-const BIND_TO: [&str; 1] = ["xilinx.com:ip:v_frmbuf_rd:2.4"];
-
 pub struct VideoFrameBufRead {
     pub hwh: hwh_parser::Ip,
     uio_acc: mem::UioAccessor<usize>,
@@ -18,8 +16,9 @@ pub struct VideoFrameBufRead {
 
 impl VideoFrameBufRead {
     pub fn new(hwh: &hwh_parser::Ip, uio_name: &str, udmabuf_name: &str) -> Result<Self> {
+        let bind_to: [&str; 1] = ["xilinx.com:ip:v_frmbuf_rd:2.4"];
         ensure!(
-            BIND_TO.iter().any(|e| e == &hwh.vlnv),
+            bind_to.iter().any(|e| e == &hwh.vlnv),
             "VideoFrameBufRead::new(): This IP is not supported. ({})",
             hwh.vlnv
         );
@@ -73,6 +72,129 @@ impl VideoFrameBufRead {
         unsafe {
             self.udmabuf_acc.copy_from(frame, 0x00, 1);
         }
+    }
+    pub fn set_format(&mut self, fmt: &str) {
+        match fmt {
+            "YUYV" => {
+                self.fmt = 12;
+                self.image_bytes_per_pix = 2;
+            },
+            "RGB8" => {
+                self.fmt = 20;
+                self.image_bytes_per_pix = 3;
+            },
+            _ => {
+                unimplemented!();
+            }
+        }
+    }
+    pub fn get_format(&self) -> &str {
+        match self.fmt {
+            12 => {
+                "YUYV"
+            },
+            20 => {
+                "RGB8"
+            },
+            _ => {
+                unimplemented!();
+            }
+        }
+    }
+    pub fn write_format(&self) {
+        unsafe {
+            self.uio_acc.write_mem32(0x10, self.image_width);
+            self.uio_acc.write_mem32(0x18, self.image_height);
+            self.uio_acc
+                .write_mem32(0x20, self.image_width * self.image_bytes_per_pix);
+            self.uio_acc.write_mem32(0x28, self.fmt);
+            println!(
+                "Width: {}, Height: {}, Stride: {}, Fmt: {}",
+                self.uio_acc.read_mem32(0x10),
+                self.uio_acc.read_mem32(0x18),
+                self.uio_acc.read_mem32(0x20),
+                self.uio_acc.read_mem32(0x28)
+            )
+        }
+    }
+}
+
+pub struct VideoFrameBufWrite {
+    pub hwh: hwh_parser::Ip,
+    uio_acc: mem::UioAccessor<usize>,
+    udmabuf_acc: mem::UdmabufAccessor<usize>,
+    fmt: u32,
+    pub image_width: u32,
+    pub image_height: u32,
+    image_bytes_per_pix: u32,
+}
+
+impl VideoFrameBufWrite {
+    pub fn new(hwh: &hwh_parser::Ip, uio_name: &str, udmabuf_name: &str) -> Result<Self> {
+        let bind_to: [&str; 1] = ["xilinx.com:ip:v_frmbuf_wr:2.4"];
+        ensure!(
+            bind_to.iter().any(|e| e == &hwh.vlnv),
+            "VideoFrameBufRead::new(): This IP is not supported. ({})",
+            hwh.vlnv
+        );
+        let uio = mem::new(uio_name)?;
+        let udmabuf = UdmabufAccessor::new(udmabuf_name, false).unwrap();
+        Ok(VideoFrameBufWrite {
+            hwh: hwh.clone(),
+            uio_acc: uio,
+            udmabuf_acc: udmabuf,
+            fmt: 12,
+            image_height: 1280,
+            image_width: 720,
+            image_bytes_per_pix: 2,
+        })
+    }
+
+    pub fn is_running(&self) -> bool {
+        unsafe { self.uio_acc.read_mem32(0x00) & 1 == 1 }
+    }
+    pub fn is_done(&self) -> bool {
+        unsafe { self.uio_acc.read_mem32(0x00) & 2 == 1 }
+    }
+    pub fn is_idle(&self) -> bool {
+        unsafe { self.uio_acc.read_mem32(0x00) & 4 == 1 }
+    }
+    pub fn is_ready(&self) -> bool {
+        unsafe { self.uio_acc.read_mem32(0x00) & 1 == 0 }
+    }
+    pub fn get_auto_restart_enable(&self) -> bool {
+        unsafe { self.uio_acc.read_mem32(0x00) & 128 == 1 }
+    }
+    pub fn set_auto_restart_enable(&self, en: bool) {
+        let reg = if en { 0x80 } else { 0 };
+        unsafe {
+            self.uio_acc.write_mem32(0x00, reg);
+        }
+    }
+    pub fn start(&self) {
+        let reg = unsafe { self.uio_acc.read_mem32(0x00) } & 0x80;
+        unsafe {
+            self.uio_acc.write_mem32(0x00, reg | 1);
+        }
+    }
+    pub fn set_framebuf_addr(&self) {
+        unsafe {
+            self.uio_acc
+                .write_mem32(0x30, self.udmabuf_acc.phys_addr() as u32);
+        }
+    }
+    pub fn read_frame(&self) -> image::RgbImage {
+        let w = self.image_width as usize;
+        let h = self.image_height as usize;
+        let bpp = self.image_bytes_per_pix as usize;
+        let mut buf = Vec::with_capacity(w * h * bpp);
+        println!("len: {}", buf.len());
+        unsafe {
+            self.udmabuf_acc.copy_to(0x00, buf.as_mut_ptr(),  w * h * bpp);
+            buf.set_len(w * h * bpp);
+        }
+        println!("len: {}", buf.len());
+        image::ImageBuffer::from_raw(self.image_width, self.image_height, buf).unwrap()
     }
     pub fn set_format(&mut self, fmt: &str) {
         match fmt {
