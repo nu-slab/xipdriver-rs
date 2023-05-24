564f035d691f7cf1074b0403a661b545aa2b322e
shinmai1054 <matsui1054@icloud.com>
2023-05-24 18:29:50 +0900

    yuyv2rgbがとりあえず動いた

---
+++ examples/yuyv2rgb.rs
@@ -0,0 +1,100 @@
+use xipdriver_rs::v_frmbuf::{VideoFrameBufRead, VideoFrameBufWrite};
+use xipdriver_rs::v_proc_ss::VideoProcSubsystemCsc;
+
+fn main() {
+    let map = xipdriver_rs::hwh_parser::parse("fc_design.hwh").unwrap();
+
+    let mut vfb_r =
+        VideoFrameBufRead::new(&map["/v_frmbuf_rd_0"], "v_frmbuf_rd", "udmabuf_vfbr").unwrap();
+
+    let mut vpss_csc = VideoProcSubsystemCsc::new(&map["/v_proc_ss_0"], "v_proc_ss").unwrap();
+
+    let mut vfb_w =
+        VideoFrameBufWrite::new(&map["/v_frmbuf_wr_0"], "v_frmbuf_wr", "udmabuf_vfbw").unwrap();
+
+    let frame_width = 1280;
+    let frame_height = 720;
+
+    // v_frmbuf_read config
+    vfb_r.frame_width = frame_width;
+    vfb_r.frame_height = frame_height;
+    vfb_r.set_format("YUYV");
+
+    // v_proc_ss config
+    vpss_csc.frame_width = frame_width;
+    vpss_csc.frame_height = frame_height;
+    vpss_csc.set_format("4:2:2", "RGB");
+
+    // v_frmbuf_write config
+    vfb_w.frame_width = frame_width;
+    vfb_w.frame_height = frame_height;
+    vfb_w.set_format("RGB8");
+
+    // start IP
+    vpss_csc.start().unwrap();
+    vfb_w.start();
+
+    for k in vpss_csc.read_csc_matrix().iter() {
+        print!("{} ", k);
+    }
+    println!();
+
+    println!("Write & Read frames");
+    let mut read_frames = Vec::new();
+    for i in 0..10 {
+        let frame: Vec<u8> = vec![0xFF / 9 * (9 - i); (frame_width * frame_height * 3) as usize];
+        let frame_yuyv = rgb2yuyv(&frame);
+
+        // Write to v_frmbuf_read
+        vfb_r.write_frame(frame_yuyv.as_ptr());
+
+        // Read from v_frmbuf_write
+        let rgb_frame = vfb_w.read_frame();
+
+        println!(
+            "Pixel(0, 0): WriteRGB: [{}, {}, {}], WriteYUYV: [{}, {}], Read: {:?}",
+            frame[0],
+            frame[1],
+            frame[2],
+            frame_yuyv[0],
+            frame_yuyv[1],
+            rgb_frame.get_pixel(0, 0)
+        );
+        read_frames.push(rgb_frame);
+    }
+
+    println!("Save frames");
+    for i in 0..read_frames.len() {
+        println!("Writing out{}.bmp...", i);
+        read_frames[i].save(format!("out{}.bmp", i)).unwrap();
+    }
+
+    println!("Done!");
+
+    vfb_r.stop();
+    vpss_csc.stop();
+    vfb_w.stop();
+}
+
+
+fn rgb2yuyv(rgb: &[u8]) -> Vec<u8> {
+    assert_eq!(rgb.len() % 3, 0);
+    let length = rgb.len() / 3;
+    let mut yuyv = vec![0; length * 2];
+    for i in 0..length {
+        let r = rgb[3 * i + 0] as f64;
+        let g = rgb[3 * i + 1] as f64;
+        let b = rgb[3 * i + 2] as f64;
+        let y = 0.257 * r + 0.504 * g + 0.098 * b + 16.0;
+        let u = -0.148 * r - 0.291 * g + 0.439 * b + 128.0;
+        let v = 0.439 * r - 0.368 * g - 0.071 * b + 128.0;
+        if i % 2 == 0 {
+            yuyv[2 * i + 0] = y.max(0.0).min(255.0) as u8;
+            yuyv[2 * i + 1] = u.max(0.0).min(255.0) as u8;
+        } else {
+            yuyv[2 * i + 0] = y.max(0.0).min(255.0) as u8;
+            yuyv[2 * i + 1] = v.max(0.0).min(255.0) as u8;
+        }
+    }
+    yuyv
+}
--- src/v_proc_ss.rs
+++ src/v_proc_ss.rs
@@ -1,4 +1,3 @@
-
 use crate::{hwh_parser, mem};
 use anyhow::{ensure, Result};
 
@@ -9,45 +8,36 @@ const BIND_TO: [&str; 1] = ["xilinx.com:ip:v_proc_ss:2.3"];
 
 macro_rules! float2sfix3_12 {
     ($float_num: expr) => {
-        if $float_num >= 0. {
-            (($float_num * 4096.).round() as u32) & 0x7FFF
-        }
-        else {
-            ((($float_num * -4096.).round() as u32) | 0x8000) & 0xFFFF
-        }
+        (($float_num * 4096.).round() as i32)
     };
 }
 
 macro_rules! sfix2f32 {
     ($fix_num: expr) => {
-        if $fix_num & 0x8000 == 0 {
-            ($fix_num & 0x7FFF) as f32 / 4096.
-        }
-        else {
-            ($fix_num & 0x7FFF) as f32 / -4096.
-        }
+        $fix_num as f32 / 4096.
     };
 }
-
 
 pub struct VideoProcSubsystemCsc {
     pub hwh: hwh_parser::Ip,
     uio_acc: mem::UioAccessor<usize>,
-    pub fmt_in: u32,
-    pub fmt_out: u32,
-    pub color_depth: u32,
-    pub brightness: u32,
-    pub contrast: i32,
-    pub saturation: u32,
-    pub red_gain: u32,
-    pub green_gain: u32,
-    pub blue_gain: u32,
-    pub red_offset: i32,
-    pub green_offset: i32,
-    pub blue_offset: i32,
-    pub clamp_min: u32,
-    pub clip_max: u32,
-    pub csc_mat: Vec<f32>,
+    pub frame_width: u32,
+    pub frame_height: u32,
+    fmt_in: u32,
+    fmt_out: u32,
+    color_depth: u32,
+    // brightness: u32,
+    contrast: i32,
+    // saturation: u32,
+    // red_gain: u32,
+    // green_gain: u32,
+    // blue_gain: u32,
+    red_offset: i32,
+    green_offset: i32,
+    blue_offset: i32,
+    clamp_min: u32,
+    clip_max: u32,
+    csc_mat: Vec<f32>,
 }
 
 impl VideoProcSubsystemCsc {
@@ -66,21 +56,23 @@ impl VideoProcSubsystemCsc {
         Ok(VideoProcSubsystemCsc {
             hwh: hwh.clone(),
             uio_acc: uio,
+            frame_width: 1280,
+            frame_height: 720,
             fmt_in: 2,
             fmt_out: 0,
             color_depth: 8,
-            brightness: 120,
+            // brightness: 120,
             contrast: 0,
-            saturation: 100,
-            red_gain: 120,
-            green_gain: 120,
-            blue_gain: 120,
-            red_offset: 0,
-            green_offset: 0,
-            blue_offset: 0,
+            // saturation: 100,
+            // red_gain: 120,
+            // green_gain: 120,
+            // blue_gain: 120,
+            red_offset: 100,
+            green_offset: 100,
+            blue_offset: 100,
             clamp_min: 0,
-            clip_max: 0,
-            csc_mat: csc_mat.clone()
+            clip_max: 255,
+            csc_mat,
         })
     }
     pub fn is_running(&self) -> bool {
@@ -105,29 +97,59 @@ impl VideoProcSubsystemCsc {
         }
     }
     pub fn start(&self) -> Result<()> {
-        self.write_fmt()?;
-        self.write_csc_matrix();
-        let reg = unsafe { self.uio_acc.read_mem32(0x00) } & 0x80;
+        self.configure()?;
         unsafe {
-            self.uio_acc.write_mem32(0x00, reg | 1);
+            self.uio_acc.write_mem32(0x00, 0x81);
         }
         Ok(())
     }
-    pub fn set_frame_size(&self, image_width: u32, image_height: u32) {
+    pub fn start_once(&self) -> Result<()> {
+        self.configure()?;
         unsafe {
-            self.uio_acc.write_mem32(0x20, image_width);
-            self.uio_acc.write_mem32(0x28, image_height);
+            self.uio_acc.write_mem32(0x00, 0x01);
+        }
+        Ok(())
+    }
+    pub fn configure(&self) -> Result<()> {
+        self.write_frame_size();
+        self.write_fmt()?;
+        self.write_csc_matrix();
+        Ok(())
+    }
+    pub fn stop(&self) {
+        self.set_auto_restart_enable(false);
+    }
+    pub fn write_frame_size(&self) {
+        unsafe {
+            self.uio_acc.write_mem32(0x20, self.frame_width);
+            self.uio_acc.write_mem32(0x28, self.frame_height);
             self.uio_acc.write_mem32(0x30, 0);
-            self.uio_acc.write_mem32(0x38, image_width-1);
+            self.uio_acc.write_mem32(0x38, self.frame_width-1);
             self.uio_acc.write_mem32(0x40, 0);
-            self.uio_acc.write_mem32(0x48, image_height-1);
+            self.uio_acc.write_mem32(0x48, self.frame_height-1);
         }
     }
-    pub fn set_mode(&mut self, src_mode: u32, dst_mode: u32) -> Result<()> {
-        ensure!(src_mode < 4, "src_mode must be in the range 1 to 3");
-        ensure!(dst_mode < 4, "dst_mode must be in the range 1 to 3");
-        match (src_mode, dst_mode) {
-            (2, 0) => { // 4:2:2 to RGB
+    pub fn set_format(&mut self, src_fmt: &str, dst_fmt: &str) {
+        match (src_fmt, dst_fmt) {
+            ("4:2:2", "RGB") | ("BT.709", "RGB") => {
+                // 4:2:2 (BT.709) to RGB
+                self.csc_mat[0] =  1.1644;
+                self.csc_mat[1] =  0.;
+                self.csc_mat[2] =  1.7927;
+                self.csc_mat[3] =  1.1644;
+                self.csc_mat[4] = -0.2132;
+                self.csc_mat[5] = -0.5329;
+                self.csc_mat[6] =  1.1644;
+                self.csc_mat[7] =  2.1124;
+                self.csc_mat[8] =  0.;
+                self.red_offset = -248;
+                self.green_offset = 77;
+                self.blue_offset = -289;
+                self.fmt_in = 2;
+                self.fmt_out = 0;
+            },
+            ("BT.601", "RGB") => {
+                // 4:2:2 (BT.601) to RGB
                 self.csc_mat[0] = 1.1644;
                 self.csc_mat[1] = 0.;
                 self.csc_mat[2] = 1.5906;
@@ -137,16 +159,65 @@ impl VideoProcSubsystemCsc {
                 self.csc_mat[6] = 1.1644;
                 self.csc_mat[7] = 2.0172;
                 self.csc_mat[8] = 0.;
-            }
+                self.red_offset = -223;
+                self.green_offset = 136;
+                self.blue_offset = -277;
+                self.fmt_in = 2;
+                self.fmt_out = 0;
+            },
+            ("RGB", "4:2:2") | ("RGB", "BT.709") => {
+                // RGB to 4:2:2 (BT.709)
+                self.csc_mat[0] =  1.1826;
+                self.csc_mat[1] =  0.6142;
+                self.csc_mat[2] =  0.0620;
+                self.csc_mat[3] = -0.1006;
+                self.csc_mat[4] = -0.3386;
+                self.csc_mat[5] =  0.4392;
+                self.csc_mat[6] =  0.4392;
+                self.csc_mat[7] = -0.3989;
+                self.csc_mat[8] = -0.0403;
+                self.red_offset = 16;
+                self.green_offset = 128;
+                self.blue_offset = 128;
+                self.fmt_in = 0;
+                self.fmt_out = 2;
+            },
+            ("RGB", "BT.601") => {
+                // RGB to 4:2:2 (BT.601)
+                self.csc_mat[0] =  0.2568;
+                self.csc_mat[1] =  0.5041;
+                self.csc_mat[2] =  0.0979;
+                self.csc_mat[3] = -0.1482;
+                self.csc_mat[4] = -0.2910;
+                self.csc_mat[5] =  0.4393;
+                self.csc_mat[6] =  0.4393;
+                self.csc_mat[7] = -0.3678;
+                self.csc_mat[8] = -0.0714;
+                self.red_offset = 16;
+                self.green_offset = 128;
+                self.blue_offset = 128;
+                self.fmt_in = 0;
+                self.fmt_out = 2;
+            },
             _ => {
-                todo!();
+                self.csc_mat[0] = 1.;
+                self.csc_mat[1] = 0.;
+                self.csc_mat[2] = 0.;
+                self.csc_mat[3] = 0.;
+                self.csc_mat[4] = 1.;
+                self.csc_mat[5] = 0.;
+                self.csc_mat[6] = 0.;
+                self.csc_mat[7] = 0.;
+                self.csc_mat[8] = 1.;
+                self.red_offset = 0;
+                self.green_offset = 0;
+                self.blue_offset = 0;
+                self.fmt_in = 0;
+                self.fmt_out = 0;
             }
         }
-        self.fmt_in = src_mode;
-        self.fmt_out = dst_mode;
         self.clamp_min = 0;
         self.clip_max = (1 << self.color_depth) - 1;
-        Ok(())
     }
     pub fn write_fmt(&self) -> Result<()> {
         ensure!(self.fmt_in < 4, "fmt_in must be in the range 1 to 3");
@@ -162,7 +233,8 @@ impl VideoProcSubsystemCsc {
     pub fn write_csc_matrix(&self) {
         unsafe {
             for i in 0..9 {
-                self.uio_acc.write_mem32(0x50 + i * 8, float2sfix3_12!(self.csc_mat[i]));
+                self.uio_acc
+                    .write_memi32(0x50 + i * 8, float2sfix3_12!(self.csc_mat[i]));
             }
             self.uio_acc.write_memi32(0x98, self.red_offset);
             self.uio_acc.write_memi32(0xa0, self.green_offset);
@@ -181,7 +253,6 @@ impl VideoProcSubsystemCsc {
         ret
     }
     pub fn set_contrast(&mut self, value: i32) -> Result<()> {
-
         let contrast = value * 4 - 200;
         let scale = 1 << (self.color_depth - 8);
         let c_diff = (contrast - self.contrast) * scale;
