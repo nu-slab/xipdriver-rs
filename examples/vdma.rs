7287d441048c73c300489d2a0d6b1da192419e2c
shinmai1054 <matsui1054@icloud.com>
2023-06-14 18:11:01 +0900

    読み書きのテスト

--- examples/vdma.rs
+++ examples/vdma.rs
@@ -1,17 +1,53 @@
+use xipdriver_rs::v_frmbuf::VideoFrameBufWrite;
+use xipdriver_rs::vdma::AxiVdmaMM2S;
+use xipdriver_rs::hwh_parser;
+use std::{thread, time};
+
 fn main() {
-    let map = xipdriver_rs::hwh_parser::parse("fc_design.hwh").unwrap();
+    let map = hwh_parser::parse("fc_design.hwh").unwrap();
 
-    let vdma = xipdriver_rs::vdma::AxiVdma::new(&map["/axi_vdma_0"], "dma", "udmabuf0").unwrap();
-    let gpio = xipdriver_rs::axigpio::AxiGpio::new(&map["/axi_gpio_0"], "gpio").unwrap();
+    let vdma_udma_names = vec!["udmabuf_vdma1", "udmabuf_vdma2", "udmabuf_vdma3"];
 
+    let mut vdma = AxiVdmaMM2S::new(&map["/axi_vdma_0"], "dma", &vdma_udma_names).unwrap();
+
+    let mut vfb_w =
+        VideoFrameBufWrite::new(&map["/v_frmbuf_wr_0"], "v_frmbuf_wr", "udmabuf_vfbw").unwrap();
+
+    let frame_width = 1280;
+    let frame_height = 720;
+
+    // vdma config
+    vdma.frame_width = frame_width;
+    vdma.frame_height = frame_height;
+    vdma.bytes_per_pix = 3;
+    vdma.pix_per_clk = 1;
+
+    // v_frmbuf_write config
+    vfb_w.frame_width = frame_width;
+    vfb_w.frame_height = frame_height;
+    vfb_w.set_format("RGB8");
+
+    // start IP
+    vfb_w.start();
     println!("is_running: {}", vdma.is_running());
-    vdma.start();
+    vdma.start().unwrap();
     println!("is_running: {}", vdma.is_running());
 
-    println!("{}", gpio.read_data(1).unwrap());
-    println!("{}", gpio.read_data(2).unwrap());
-    gpio.write_data(2, 123456).unwrap();
-    println!("{}", gpio.read_data(2).unwrap());
+    for i in 0..10 {
+        let frame: Vec<u8> = vec![0xFF / 9 * (9 - i); (frame_width * frame_height * 3) as usize];
+        vdma.write_frame(frame.as_ptr()).unwrap();
+        let start = time::Instant::now();
+        let rgb_frame = vfb_w.read_frame_as_image();
+        let end = start.elapsed();
+        println!("{}秒経過しました。", end.as_secs_f32());
+        rgb_frame.save(format!("out{}.bmp", i)).unwrap();
+    }
+
+    for _ in 0..100 {
+        println!("{}", vdma.read_active_frame());
+        thread::sleep(time::Duration::from_millis(10));
+    }
 
     vdma.reset();
+    vfb_w.stop();
 }
