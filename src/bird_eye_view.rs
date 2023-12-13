77a8e19d9410e1f2f2458196f027471cbaea8134
shinmai1054 <46531958+shinmai1054@users.noreply.github.com>
2023-12-14 03:23:45 +0900

    エラーが出るので勝手に修正しておきました

--- src/bird_eye_view.rs
+++ src/bird_eye_view.rs
@@ -117,7 +117,7 @@ impl BirdEyeViewHW {
 
     pub fn write_img_in(&mut self, img_in: &[u32]) -> Result<()> {
         ensure!(
-            img_in.len() <= self.udmabuf_acc[0].size,
+            img_in.len() <= self.udmabuf_acc[0].size(),
             "img_in.len() too large"
         );
         unsafe {
@@ -128,7 +128,7 @@ impl BirdEyeViewHW {
 
     pub fn write_img_map(&mut self, img_map: &[u32]) -> Result<()> {
         ensure!(
-            img_map.len() <= self.udmabuf_acc[1].size,
+            img_map.len() <= self.udmabuf_acc[1].size(),
             "img_map.len() too large"
         );
         unsafe {
