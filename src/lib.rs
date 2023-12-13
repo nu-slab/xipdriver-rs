931ed5feb1eaac07bb504f0f701d1104c7b70e2b
nu-ken-yu <ok092477@gmail.com>
2023-12-13 12:15:01 +0900

    鳥瞰変換HWドライバー追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-pub mod vdma;
+pub mod axidma;
 pub mod axigpio;
+pub mod axis_switch;
+pub mod bird_eye_view;
+pub mod hwinfo;
+pub mod umv_lane_detector;
+pub mod umv_motor_controller;
 pub mod v_frmbuf;
 pub mod v_proc_ss;
-pub mod umv_lane_detector;
-pub mod hwinfo;
-pub mod umv_motor_controller;
-pub mod axidma;
+pub mod vdma;
 pub mod yolo;
-pub mod axis_switch;
